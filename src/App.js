7fe9ed0bc3a848574939cc86aa58791735a097eb
Henrik Hansen <lianides@hotmail.com>
2020-11-22 23:05:23 +0100

    Til og med video 17. Alt virker som det skal.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
-import React from "react";
+import React, {useState, useRef} from "react";
+//Import styles
+import "./styles/app.scss";
+//Adding components
+import Player from './components/Player';
+import Song from './components/Song';
+import Library from './components/Library';
+import Nav from './components/Nav';
+//Import Data
+import Data from "./data";
 
 function App() {
+  //Ref
+  const audioRef = useRef(null);
+  //State
+  const [songs, setSongs] = useState(Data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [songInfo, setSongInfo] = useState({
+    currentTime : 0,
+    duration: 0, 
+ });
+ const [libraryStatus, setLibraryStatus] = useState(false);
+ const timeUpdateHandler = (e) => {
+  const current = e.target.currentTime;
+  const duration = e.target.duration;
+  setSongInfo({
+      ...songInfo, currentTime: current, duration
+  });
+};
   return (
     <div className="App">
-     <h1>Music Player</h1>
-     virker det denne vej
-     måske også den anden vej
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
+      <Song currentSong={currentSong} />
+      <Player
+        audioRef={audioRef}
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+        setSongInfo={setSongInfo}
+        songInfo={songInfo} 
+      />
+      <Library 
+        audioRef={audioRef} 
+        songs={songs} 
+        setCurrentSong={setCurrentSong}
+        isPlaying={isPlaying}
+        setSongs={setSongs}
+        libraryStatus={libraryStatus} 
+      />
+      <audio
+                onTimeUpdate={timeUpdateHandler}
+                onLoadedMetadata={timeUpdateHandler} 
+                ref={audioRef} 
+                src={currentSong.audio}>
+      </audio> 
     </div>
   );
 }
---
+++ src/components/LibrarySong.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+const LibrarySong = ({song, songs, setCurrentSong, id, audioRef, isPlaying, setSongs}) => {
+    const songSelectHandler = () => {
+        //This is not nessesary course we have the song id allready, 
+        //therefor setCurrentSong(song);
+        //const selectedSong = songs.filter((state) => state.id === id); 
+        //setCurrentSong(selectedSong[0]);
+        setCurrentSong(song);
+        //Add Active State class
+        const newSongs = songs.map((song) => {
+            if(song.id === id) {
+                return{
+                    ...song, active: true,
+                };
+            }else{
+                return{
+                    ...song, active: false,
+                }
+            }
+        });
+        setSongs(newSongs);
+        //Check if the song is playing
+        if(isPlaying){
+            const playPromise = audioRef.current.play();
+            if(playPromise !== undefined){
+                playPromise.then((audio) => {
+                    audioRef.current.play();
+                })
+            }
+        }
+        
+    }
+    return(
+        <div onClick={songSelectHandler} className={`library-song ${song.active ? 'selected' : ""}`}>
+            <img src={song.cover} alt={song.name}/>
+            <div className="song-description">
+            <h3>{song.name}</h3>
+            <h4>{song.artist}</h4>
+            </div>           
+        </div>
+    );
+};
+
+export default LibrarySong;
